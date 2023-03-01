70248095a735fff83dfa4259d4b7248a06f4e812
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-03-01 23:55:50 +0200

    hw_23_55

--- My_HW.py
+++ My_HW.py
@@ -1,8 +1,18 @@
-user_database = [
-]
-register_data = " "
-user_name = input("Enter your name : ")
-if len(user_name) > 5:
-    print("good")
+
+print("Hello user, if you want to register, please provide the following data")
+
+database = []
+
+register_data = {
+    "name" : input("Enter your name (login length less than 5 characters) : "),
+    "email" : input("Enter your email : "),
+    "password" : input("Enter your password (password contains more than 10 symbols) : ")
+}
+
+if (len(register_data["name"])) < 5 or (len(register_data["password"])) > 10:
+    print("Your name or password is incorret. Try again")
 else:
-    print("bad")
+    print("Thank you, you have successfully registered")
+
+database.append(register_data)
+print(database)
