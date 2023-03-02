62f383ef6d25836a66253c935849a5116bcd1a4b
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-03-02 11:51:26 +0200

    help me

--- My_HW.py
+++ My_HW.py
@@ -1,18 +1,19 @@
-
 print("Hello user, if you want to register, please provide the following data")
 
 database = []
 
-register_data = {
+register_data = ""
+
+user= {
     "name" : input("Enter your name (login length less than 5 characters) : "),
     "email" : input("Enter your email : "),
     "password" : input("Enter your password (password contains more than 10 symbols) : ")
 }
 
-if (len(register_data["name"])) < 5 or (len(register_data["password"])) > 10:
+if (len(user["name"])) < 5 or (len(user["password"])) > 10:
     print("Your name or password is incorret. Try again")
 else:
     print("Thank you, you have successfully registered")
 
-database.append(register_data)
+database.append(user)
 print(database)
